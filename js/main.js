047425da6f47a57a0f1384a9b3a87cb9fffe5578
Yurii <eazylifemusic@gmail.com>
2023-03-17 15:32:59 +0600

    Сделал раздел с проектами

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-  const block = document.querySelector('[data-block]'),
-        btnForBlock =document.querySelector('[data-btn]');
+  const block = document.querySelectorAll('[data-block]'),
+        btnForBlock =document.querySelectorAll('[data-btn]');
 
-  btnForBlock.addEventListener('click', () => {
-    block.classList.toggle('projects__inner--open');
-    if (block.classList.contains('projects__inner--open')) {
-      btnForBlock.style.paddingTop = '10px';
-      btnForBlock.style.borderRadius = '15px';
-      btnForBlock.classList.add('projects__btn--open');
-      block.style.bottom = '-96%';
-    } else {
-      btnForBlock.style.paddingTop = '';
-      btnForBlock.style.borderRadius = '';
-      block.style.bottom = '';
-      btnForBlock.classList.remove('projects__btn--open');
-    }
- });
+  btnForBlock.forEach((item, i) => {
+    item.addEventListener('click', () => {
+      block[i].classList.toggle('projects__inner--open');
+      if (block[i].classList.contains('projects__inner--open')) {
+        item.textContent = 'Скрыть описание';
+      } else {
+        item.textContent = 'Описание проекта';
+      }
+    });
+  });
 });
